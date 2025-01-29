c3217794a56d43c72a1c234fff75f689944631d5
Kirby <kirbdog315@gmail.com>
2025-01-29 00:25:21 -0600

    added initial working javascript search function

--- mangadex-api.js
+++ mangadex-api.js
@@ -3,15 +3,17 @@ const MANGADEX_API_BASE = 'https://api.mangadex.org'
 
 async function searchManga(title) {
     try {
-        const searchUrl = new URL(`${MANGADEX_API_BASE}`/manga);
+        const searchUrl = new URL('/manga', MANGADEX_API_BASE);
         searchUrl.searchParams.append('title', title);
         searchUrl.searchParams.append('limit', 5);
         searchUrl.searchParams.append('order[relevance]', 'desc');
 
+        console.log('Attempting to fetch:', searchUrl.toString()); // Debug Log
+
         // Make API call
         const response = await fetch(searchUrl.toString());
         if(!response.ok){
-            throw new Error(`HTTP erro! status: ${response.status}`);
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
 
         const data = await response.json();
